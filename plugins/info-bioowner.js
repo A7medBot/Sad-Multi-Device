765df05f4ecd1917ba285b7b9c8af618a53cb9fe
A7medBot <105971100+A7medBot@users.noreply.github.com>
2022-06-14 15:32:05 +0300

    Update info-bioowner.js

--- plugins/info-bioowner.js
+++ plugins/info-bioowner.js
@@ -1,24 +1,20 @@
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 let ppown = await conn.profilePictureUrl(nomorown + '@s.whatsapp.net', 'image').catch(_ => hwaifu[1]) 
-let teksbio = `*───────[ BIODATA OWNER ]───────*
-*💌 Nama* : Sad-Bot
-*✉️ Nama RL* : Fahri
-*♂️ Gender* : Laki - laki
-*🕋 Agama* : Islam
-*⏰ Tanggal lahir* : 10 Juni 2007
-*🎨 Umur* : 15
-*🧮 Kelas* : 9
-*🧩 Hobby* : Nonton Hanime, Chatting, Recode script bot
-*💬 Sifat* : Idiot, Tidak Ramah, Kanjut, Prik, Pedofil
-*🗺️ Tinggal* : Indo, Riau, Indragiri Hulu, Air Molek
-*❤️ Suka* : warnah kuning & biru, Hanime, waifu, loli, trap, furry, kucing
-*💔 Benci* : autis, anak epep, seleb
-
-*───────[ SOSIAL MEDIA ]───────*
-*📷 instagran* : @bot_whangsaf
-*🇫  Facebook* : Adison Fahri
-*🏮 Chanel Youtube* : Papah-Chan
-*🐈 Github:* FahriAdison
+let teksbio = `*───────[ ABOUT OWNER ]───────*
+*💌 Name* : Potato
+*✉️ Name RL* : Ahmed
+*♂️ Gender* : male - male
+*🕋 Religion* : Islam
+*⏰ Birthday* : 23 April 2005
+*🎨 Age* : 17
+*🧮 Class* : 6th preparatory
+*🧩 Hobby* : Gaming, Chatting, script bot
+*💬 Nature* : Idiot, Not friendly
+*🗺️ live* : Basrah,Iraq
+*───────[ SOCIAL MEDIA ]───────*
+*📷 instagram* : @lhjj_56
+*F.  Facebook* : AĦMED ALHUJAJ
+*D. Discord* : I_A7med_XD#3168
 
 `
 conn.sendHydrated(m.chat, teksbio, wm, ppown, "wa.me/" + nomorown, "💬 ᴄʜᴀᴛs", null,null, [["ᴅᴏɴᴀsɪ", '.donasi'], [null, null],[null,null]], m)
